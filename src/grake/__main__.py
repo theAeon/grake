602b7e4ecf1aba5e6e046008d615557c59faa380
Andrew Robbins <andrew@robbinsa.me>
2022-08-30 14:39:11 -0400

    bugfix

--- src/grake/__main__.py
+++ src/grake/__main__.py
@@ -6,22 +6,23 @@ from .usagebyuser import graphcmd, topcmd
 @click.group()
 @click.option('-j', '--threads', default=1, help="number of threads", metavar='threads', type=int)
 @click.pass_context
-def cli(ctx, thread):
-    ctx.obj['thread'] = thread
+def cli(ctx, threads):
+    ctx.obj = {}
+    ctx.obj['threads'] = threads
 
 
 @cli.command()
-@click.argument('filename', type=click.Path(exists=True), help="Text output of DWalk, sorted by user", required=True)
+@click.argument('filename', type=click.Path(exists=True), required=True)
 @click.pass_context
-def graph(ctx, file):
+def graph(ctx, filename):
     """compare usage by user"""
-    graphcmd(ctx.thread, file)
+    graphcmd(ctx.obj['threads'], filename)
 
 
 @cli.command()
-@click.argument('filename', type=click.Path(exists=True), help="Text output of DWalk, sorted by user", required=True)
-@click.argument('user', nargs=1, help="a unix user", required=True)
+@click.argument('filename', type=click.Path(exists=True), required=True)
+@click.argument('user', nargs=1, required=True)
 @click.pass_context
-def top(ctx, file, user):
+def top(ctx, filename, user):
     """get largest directories for user"""
-    topcmd(ctx.thread, file, user)
+    topcmd(ctx.obj['threads'], filename, user)
